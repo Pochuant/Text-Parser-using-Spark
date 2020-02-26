5f387e66cdc991869b83669991193034129fb2b1
pochuan <pochuant@usc.edu>
2020-02-25 23:14:10 -0800

    add a parser class

--- parser_func.py
+++ parser_func.py
@@ -1,11 +1,12 @@
-def flatten_dict(structure):
-    output={}
-    for k, v in structure.items():
-      if isinstance(v, dict):
-        tmp = flatten_dict(v)
-        for k1, v1 in tmp.items():
-          new_key = k + '.' + k1
-          output[new_key] = v1
-      else:
-        output[k] = v
-    return output
+class parser_func:
+  def flatten_dict(structure):
+      output={}
+      for k, v in structure.items():
+        if isinstance(v, dict):
+          tmp = flatten_dict(v)
+          for k1, v1 in tmp.items():
+            new_key = k + '.' + k1
+            output[new_key] = v1
+        else:
+          output[k] = v
+      return output
