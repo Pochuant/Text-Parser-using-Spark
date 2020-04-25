f2ea11e6a669dc728b39fdb8be46142d273a7eda
pochuan <pochuant@usc.edu>
2020-04-25 11:28:09 -0700

    add flatten list function

--- parser_func.py
+++ parser_func.py
@@ -1,12 +1,25 @@
+dict_output = {}
+list_output = []
+
 class parser_func:
-  def flatten_dict(structure):
-      output={}
-      for k, v in structure.items():
-        if isinstance(v, dict):
-          tmp = flatten_dict(v)
-          for k1, v1 in tmp.items():
-            new_key = k + '.' + k1
-            output[new_key] = v1
-        else:
-          output[k] = v
-      return output
+  def flatten_dict(input_dic):
+    for k,v in input_dic.items():
+      if isinstance(v, str):
+        dict_output[k] = v
+      elif isinstance(v, dict):
+        parser_func.flatten_dict({k+'.'+ i:j for i,j in v.items()})
+      elif isinstance(v, list):
+        dict_output[k] = []
+        for listelement in v:
+          if isinstance(listelement,str):
+            dict_output[k].append(listelement)
+          elif isinstance(listelement, dict):
+            parser_func.flatten_dict({k+'.'+ i : j for i,j in listelement.items()})
+
+
+  def flatten_list(input_list):
+    for i in input_list:
+      if isinstance(i, int):
+        list_output.append(i)
+      elif isinstance(i, list):
+        parser_func.flatten_list(i)
